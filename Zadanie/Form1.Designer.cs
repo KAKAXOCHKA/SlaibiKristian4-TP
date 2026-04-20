b8c87f0056b3acda6a63d54fb330313967388609
KAKAXOCHKA <kristianslaibi69@gmail.com>
2026-04-20 18:29:21 +0200

    Add files via upload

---
+++ Zadanie/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Zadanie
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxPriority = new System.Windows.Forms.ComboBox();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(122, 130);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(140, 20);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // comboBoxPriority
+            // 
+            this.comboBoxPriority.FormattingEnabled = true;
+            this.comboBoxPriority.Items.AddRange(new object[] {
+            "Ważne",
+            "Standardowe",
+            "Mało ważne"});
+            this.comboBoxPriority.Location = new System.Drawing.Point(330, 130);
+            this.comboBoxPriority.Name = "comboBoxPriority";
+            this.comboBoxPriority.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPriority.TabIndex = 4;
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.Location = new System.Drawing.Point(178, 303);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(238, 212);
+            this.listBoxTasks.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.buttonAdd.Location = new System.Drawing.Point(122, 230);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 32);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Dodaj";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(253, 230);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 32);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Usuń";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.buttonSave.Location = new System.Drawing.Point(396, 230);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 32);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Zapisz";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(585, 605);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.comboBoxPriority);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.BackColor = System.Drawing.Color.LightGray;
+
+        }
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxPriority;
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
---
+++ Zadanie/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Zadanie
+{
+    public partial class Form1 : Form
+    {
+        List<TaskItem> tasks = new List<TaskItem>();
+        string filePath = "tasks.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxPriority.Items.AddRange(new string[]
+            {
+                "Ważne",
+                "Standardowe",
+                "Mało ważne"
+            });
+
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(';');
+
+                    if (parts.Length == 2)
+                    {
+                        TaskItem task = new TaskItem
+                        {
+                            Name = parts[0],
+                            Priority = parts[1]
+                        };
+
+                        tasks.Add(task);
+                        listBoxTasks.Items.Add(task);
+                    }
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                MessageBox.Show("Wprowadź nazwę zadania!");
+                return;
+            }
+
+            if (comboBoxPriority.SelectedItem == null)
+            {
+                MessageBox.Show("Wybierz priorytet!");
+                return;
+            }
+
+            TaskItem task = new TaskItem
+            {
+                Name = textBoxName.Text,
+                Priority = comboBoxPriority.SelectedItem.ToString()
+            };
+
+            tasks.Add(task);
+            listBoxTasks.Items.Add(task);
+
+            textBoxName.Clear();
+            comboBoxPriority.SelectedIndex = -1;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedItem is TaskItem selectedTask)
+            {
+                tasks.Remove(selectedTask);
+                listBoxTasks.Items.Remove(selectedTask);
+            }
+            else
+            {
+                MessageBox.Show("Wybierz zadanie do usunięcia!");
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            List<string> lines = tasks
+                .Select(t => $"{t.Name};{t.Priority}")
+                .ToList();
+
+            File.WriteAllLines(filePath, lines);
+
+            MessageBox.Show("Zapisano!");
+        }
+    }
+}
